32be569cdf282038fe3d3280e2ed6a4d44d3bb54
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-14 01:13:38 -0300

    Update Pessoa class with validations

--- src/main/java/br/com/zup/edu/personmanager/model/Pessoa.java
+++ src/main/java/br/com/zup/edu/personmanager/model/Pessoa.java
@@ -1,37 +1,54 @@
 package br.com.zup.edu.personmanager.model;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.LocalDate;
+import javax.persistence.Table;
+import javax.validation.constraints.PastOrPresent;
+
+import org.hibernate.validator.constraints.br.CPF;
 
 @Entity
+@Table(name = "pessoas")
 public class Pessoa {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
+
+    @Column(nullable = false)
+    @CPF
     private String cpf;
+
     private String apelido;
+
+    @Column(nullable = false)
+    @PastOrPresent
     private LocalDate dataNascimento;
 
-    public Pessoa(String nome, String cpf, String apelido, LocalDate dataNascimento) {
+    /**
+     * @deprecated Construtor de uso exclusivo do Hibernate
+     */
+    @Deprecated
+    public Pessoa() {}
+
+    public Pessoa(String nome, @CPF String cpf, String apelido,
+                  @PastOrPresent LocalDate dataNascimento) {
         this.nome = nome;
         this.cpf = cpf;
         this.apelido = apelido;
         this.dataNascimento = dataNascimento;
     }
 
-    /**
-     * @deprecated
-     */
-    @Deprecated
-    public Pessoa() {
-    }
-
     public Long getId() {
         return id;
     }
+
 }
